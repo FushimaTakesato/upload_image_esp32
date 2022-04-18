32ad5576e449eafe0a4ced8cadada99bcc550dc2
FushimaTakesato <takenokonosato.f@gmail.com>
2022-04-19 04:54:08 +0900

    ArduinoIDEのESP32パッケージ標準のCameraWebServerからcamera_pins.hを使用するように変更

--- upload_image_esp32.ino
+++ upload_image_esp32.ino
@@ -1,4 +1,3 @@
-
 #include "esp_camera.h"
 #include "soc/soc.h"          
 #include "soc/rtc_cntl_reg.h"  
@@ -9,24 +8,20 @@
 #include <WiFiUdp.h>
 #include "time.h"
 #include "src/credential_define.h"
+
+// Select camera model
+#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+#include "src/camera_pins.h"
+
 ESP32_FTPClient ftp (FTP_SERVER, FTP_USER, FTP_PASS, 50000, 2);
-// Pin definition for CAMERA_MODEL_WROVER_KIT
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
+
 camera_config_t config;
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
